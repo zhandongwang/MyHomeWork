6150067aa50a89b355a4fe4b68bdc9e531ea85cc
zhandongwang <zhandong_wang@163.com>
2016-09-25 21:58:59 +0800

    Add files via upload

---
+++ MyHomeWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyHomeWork
+//
+//  Created by 王战东 on 16/9/25.
+//  Copyright © 2016年 zhandongwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyHomeWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyHomeWork
+//
+//  Created by 王战东 on 16/9/25.
+//  Copyright © 2016年 zhandongwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
