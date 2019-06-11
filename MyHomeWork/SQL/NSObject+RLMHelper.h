55f747f92d3ef27a3313678427e5e735b11f5624
fengli <fengli@2dfire.com>
2019-06-11 18:58:37 +0800

    feat:test

--- MyHomeWork/SQL/NSObject+RLMHelper.h
+++ MyHomeWork/SQL/NSObject+RLMHelper.h
@@ -10,46 +10,15 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-/**
- Type encoding's type.
- */
-typedef NS_OPTIONS(NSUInteger, CCDEncodingType) {
-    CCDEncodingTypeMask       = 0xFF, ///< mask of type value
-    CCDEncodingTypeUnknown    = 0, ///< unknown
-    CCDEncodingTypeVoid       = 1, ///< void
-    CCDEncodingTypeBool       = 2, ///< bool
-    CCDEncodingTypeInt8       = 3, ///< char / BOOL
-    CCDEncodingTypeUInt8      = 4, ///< unsigned char
-    CCDEncodingTypeInt16      = 5, ///< short
-    CCDEncodingTypeUInt16     = 6, ///< unsigned short
-    CCDEncodingTypeInt32      = 7, ///< int
-    CCDEncodingTypeUInt32     = 8, ///< unsigned int
-    CCDEncodingTypeInt64      = 9, ///< long long
-    CCDEncodingTypeUInt64     = 10, ///< unsigned long long
-    CCDEncodingTypeFloat      = 11, ///< float
-    CCDEncodingTypeDouble     = 12, ///< double
-    CCDEncodingTypeLongDouble = 13, ///< long double
-    CCDEncodingTypeObject     = 14, ///< id
-    CCDEncodingTypeClass      = 15, ///< Class
-    CCDEncodingTypeSEL        = 16, ///< SEL
-    CCDEncodingTypeBlock      = 17, ///< block
-    CCDEncodingTypePointer    = 18, ///< void*
-    CCDEncodingTypeStruct     = 19, ///< struct
-    CCDEncodingTypeUnion      = 20, ///< union
-    CCDEncodingTypeCString    = 21, ///< char*
-    CCDEncodingTypeCArray     = 22, ///< char[10] (for example)
-};
-
-
 @interface NSObject (RLMHelper)
 
 - (id)ccd_realmModelByClass:(Class)cls;
 
-
 @end
 
-@protocol RLMHelper <NSObject>
+@protocol RLMHelperProtocol <NSObject>
 
+@optional
 + (nullable NSDictionary<NSString *, NSString*> *)modelCustomRLMPropertyMapper;
 
 + (nullable NSDictionary<NSString *, Class> *)modelContainerRLMPropertyGenericClass;
--- MyHomeWork/SQL/NSObject+RLMHelper.m
+++ MyHomeWork/SQL/NSObject+RLMHelper.m
@@ -8,50 +8,8 @@
 
 #import "NSObject+RLMHelper.h"
 #import <objc/runtime.h>
-#import "FLPersonRealmModel.h"
 #import <YYModel/YYClassInfo.h>
-
-CCDEncodingType CCDEncodingGetType(const char *typeEncoding) {
-    char *type = (char *)typeEncoding;
-    if (!type) {
-        return CCDEncodingTypeUnknown;
-    }
-    size_t len = strlen(type);
-    if (len == 0) {
-        return CCDEncodingTypeUnknown;
-    }
-    switch (*type) {
-            case 'v': return CCDEncodingTypeVoid;
-            case 'B': return CCDEncodingTypeBool;
-            case 'c': return CCDEncodingTypeInt8;
-            case 'C': return CCDEncodingTypeUInt8;
-            case 's': return CCDEncodingTypeInt16;
-            case 'S': return CCDEncodingTypeUInt16;
-            case 'i': return CCDEncodingTypeInt32;
-            case 'I': return CCDEncodingTypeUInt32;
-            case 'l': return CCDEncodingTypeInt32;
-            case 'L': return CCDEncodingTypeUInt32;
-            case 'q': return CCDEncodingTypeInt64;
-            case 'Q': return CCDEncodingTypeUInt64;
-            case 'f': return CCDEncodingTypeFloat;
-            case 'd': return CCDEncodingTypeDouble;
-            case 'D': return CCDEncodingTypeLongDouble;
-            case '#': return CCDEncodingTypeClass;
-            case ':': return CCDEncodingTypeSEL;
-            case '*': return CCDEncodingTypeCString;
-            case '^': return CCDEncodingTypePointer;
-            case '[': return CCDEncodingTypeCArray;
-            case '(': return CCDEncodingTypeUnion;
-            case '{': return CCDEncodingTypeStruct;
-            case '@': {
-                if (len == 2 && *(type + 1) == '?')
-                return CCDEncodingTypeBlock;
-                else
-                return CCDEncodingTypeObject;
-            }
-        default: return CCDEncodingTypeUnknown ;
-    }
-}
+#import "FLPersonRealmModel.h"
 
 @implementation NSObject (RLMHelper)
 
@@ -61,15 +19,14 @@ CCDEncodingType CCDEncodingGetType(const char *typeEncoding) {
     
     
     Class cls = [self class];
-    NSDictionary<NSString *, Class> *genericMapper = nil;
     NSDictionary<NSString *, NSString *> *customRLMPropertyMapper = nil;
+    NSDictionary<NSString *, Class> *genericMapper = nil;
     
-    if ([cls respondsToSelector:@selector(modelCustomRLMPropertyMapper)]) {
+    if ([cls conformsToProtocol:@protocol(RLMHelperProtocol)] && [cls respondsToSelector:@selector(modelCustomRLMPropertyMapper)]) {
         customRLMPropertyMapper = [cls modelCustomRLMPropertyMapper];
     }
-    
     //数组属性
-    if ([cls respondsToSelector:@selector(modelContainerRLMPropertyGenericClass)]) {
+    if ([cls conformsToProtocol:@protocol(RLMHelperProtocol)] && [cls respondsToSelector:@selector(modelContainerRLMPropertyGenericClass)]) {
         genericMapper = [cls modelContainerRLMPropertyGenericClass];
     }
 
