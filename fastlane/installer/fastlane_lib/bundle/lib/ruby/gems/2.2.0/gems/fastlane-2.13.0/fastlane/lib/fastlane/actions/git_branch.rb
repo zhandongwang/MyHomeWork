7f15ee643cdae88f8aac406ffd3d2266f7211d97
fengli <fengli@2dfire.com>
2019-07-03 17:08:20 +0800

    feat:fastlane

---
+++ fastlane/installer/fastlane_lib/bundle/lib/ruby/gems/2.2.0/gems/fastlane-2.13.0/fastlane/lib/fastlane/actions/git_branch.rb
@@ -0,0 +1,54 @@
+module Fastlane
+  module Actions
+    module SharedValues
+    end
+
+    class GitBranchAction < Action
+      def self.run(params)
+        return ENV['GIT_BRANCH'] if FastlaneCore::Env.truthy?('GIT_BRANCH')
+        return ENV['BRANCH_NAME'] if FastlaneCore::Env.truthy?('BRANCH_NAME')
+        return ENV["TRAVIS_BRANCH"] if FastlaneCore::Env.truthy?("TRAVIS_BRANCH")
+        return ENV["BITRISE_GIT_BRANCH"] if FastlaneCore::Env.truthy?("BITRISE_GIT_BRANCH")
+        `git symbolic-ref HEAD --short 2>/dev/null`.strip
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        "Returns the name of the current git branch"
+      end
+
+      def self.details
+        "If no branch could be found, this action will return nil"
+      end
+
+      def self.available_options
+        []
+      end
+
+      def self.output
+        []
+      end
+
+      def self.authors
+        ["KrauseFx"]
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+
+      def self.example_code
+        [
+          'git_branch'
+        ]
+      end
+
+      def self.category
+        :source_control
+      end
+    end
+  end
+end
